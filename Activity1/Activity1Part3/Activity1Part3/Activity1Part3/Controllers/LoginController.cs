9269837652ba8429a3626aec2b06e177405bab1a
root <root@LAPTOP-VQSLNR9C.localdomain>
2021-04-26 02:01:05 -0500

    init commit :fire: - wk 1

---
+++ Activity1/Activity1Part3/Activity1Part3/Activity1Part3/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Activity1Part3.Models;
+using Activity1Part3.Services.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity1Part3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+            // return "Here " + model.Username + " and " + model.Password;
+            SecurityService service = new SecurityService();
+
+            bool authenticated = service.Authenticate(model);
+
+            if (authenticated) return View("LoginPassed", model);
+            return View("LoginFailed");
+        }
+
+    }
+}
---
+++ Activity1/Activity1Part3/Activity1Part3/Activity1Part3/Services/Data/SecurityDAO.cs
@@ -0,0 +1,33 @@
+﻿using Activity1Part3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Activity1Part3.Services.Data
+{
+    public class SecurityDAO
+    {
+        public bool FindByUser(UserModel user)
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                // Do work here; connection closed on following line.
+                using (SqlCommand command = new SqlCommand("SELECT TOP 1 * FROM Users WHERE USERNAME = " + user.Username + " AND PASSWORD = " + user.Password, connection))
+                // TODO: Fix - SQL Thinks the model props are column names...
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    // Return successful if any lines exist
+                    while (reader.Read())
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        }
+    }
+}
